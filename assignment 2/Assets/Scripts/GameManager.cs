1575efb5cabc539c9b16d2b7b9ca2ab2d802e532
wridzer <wridzer12@gmail.com>
2021-09-21 10:11:04 +0200

    initial commit

---
+++ assignment 2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private InputHandler inputHandler;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputHandler = new InputHandler();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputHandler.HandleInput();
+    }
+}
---
+++ assignment 2/Assets/Scripts/InputHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler
+{
+    private Dictionary<KeyCode, ICommand> keyCommands = new Dictionary<KeyCode, ICommand>();
+
+    public void HandleInput()
+    {
+        foreach(var KeyCode in keyCommands)
+        {
+            if (Input.GetKeyDown(KeyCode.Key))
+            {
+                KeyCode.Value.Execute();
+            }
+        }
+    }
+
+    private void Start()
+    {
+        ComDoThings DoThings = new ComDoThings();
+        keyCommands.Add(KeyCode.E, DoThings);
+    }
+
+    //commands interface and classes
+
+    //add and remove commands
+}
